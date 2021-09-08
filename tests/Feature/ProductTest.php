1aaf9008b9f693607e13784fe1f8fda99b679edd
Trafford Fewster <contact@trafford.dev>
2021-09-08 12:02:33 +0100

    Add laravel crud tests

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -33,4 +33,31 @@ class ProductTest extends TestCase
         $tm = Product::firstWhere('name','Training Minutes');
         $this->assertEquals(5.99,$tm->price);
     }
+
+    public function test_add()
+    {
+        $data = ['name' => 'Testing Minutes', 'price' => '10.80'];
+
+        $this->assertDatabaseMissing('products', $data);
+        $response = $this->post("/products", $data);
+        $this->assertDatabaseHas('products', $data);
+
+    }
+
+    public function test_remove()
+    {
+        $this->assertDatabaseHas('products', ['id' => 1]);
+        $response = $this->delete("/products/1");
+        $this->assertDatabaseMissing('products', ['id' => 1]);
+    }
+
+    public function test_edit()
+    {
+        $data = ['name' => 'DevMinutes', 'price' => '35.99'];
+        $dataEdited = ['name' => 'DevMinutes', 'price' => '45.99'];
+        $this->assertDatabaseHas('products', $data);
+        $response = $this->put('/products/1', $dataEdited);
+        $this->assertDatabaseHas('products', $dataEdited);
+        $this->assertDatabaseMissing($data);
+    }
 }
