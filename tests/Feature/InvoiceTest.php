4cbcdd5d885c99774feb363c0ad573db2c03f265
Trafford Fewster <contact@trafford.dev>
2021-09-16 13:42:38 +0100

    Update tests

--- tests/Feature/InvoiceTest.php
+++ tests/Feature/InvoiceTest.php
@@ -8,13 +8,13 @@ use Tests\TestCase;
 
 class InvoiceTest extends TestCase
 {
-
+    use RefreshDatabase;
     public function setUp(): void
     {
         parent::setUp();
 
         // seed the database
-        $this->artisan('db:seed');
+       $this->seed();
     }
     /**
      * A basic feature test example.
@@ -29,17 +29,18 @@ class InvoiceTest extends TestCase
 
     public function test_create()
     {
-        $data = ['billToName' => 'John Smith', 
+        $data = ['billToName' => 'Steve Smith', 
         'billToAddr1' => '2 Court Square',
         'billToAddr2' => 'New York NY 12210',
-        'dueDate' => '10/09/2021' ];
+        'dueDate' => '10/09/2021',
+        'products' => '[]' ];
         $response = $this->post("/invoice", $data);
-        $this->assertDatabaseHas('invoices', $data);
+        $this->assertDatabaseHas('invoices', ['billToName' => 'Steve Smith']);
     }
 
     public function test_remove()
     {
-        $data = ['billToName' => 'John Smith', 
+        $data = ['billToName' => 'Steve Smith', 
         'billToAddr1' => '2 Court Square',
         'billToAddr2' => 'New York NY 12210',
         'dueDate' => '10/09/2021' ];
@@ -51,18 +52,19 @@ class InvoiceTest extends TestCase
 
     public function test_edit()
     {
-        $data = ['billToName' => 'John Smith', 
+        $data = ['billToName' => 'Steve Smith', 
         'billToAddr1' => '2 Court Square',
         'billToAddr2' => 'New York NY 12210',
-        'dueDate' => '10/09/2021' ];
-        $dataUpdated = ['billToName' => 'John Bob', 
+        'dueDate' => '2022-09-10',
+        'products' => '[]' ];
+        $dataUpdated = ['billToName' => 'Steve Bob', 
         'billToAddr1' => '3 Court Square',
         'billToAddr2' => 'New York NY 14210',
-        'dueDate' => '10/09/2022' ];
+        'dueDate' => '2022-09-20' ];
         $response = $this->post("/invoice", $data);
-        $this->assertDatabaseHas('invoices', $data);
-        $response = $this->put("/invoice/1",$dataUpdated);
-        $this->assertDatabaseHas('invoices',$dataUpdated);
-        $this->assertDatabaseMissing('invoices', $data);
+        $this->assertDatabaseHas('invoices', ['billToName' => 'Steve Smith']);
+        $response = $this->put("/invoice/4",$dataUpdated);
+        $this->assertDatabaseHas('invoices',['billToName' => 'Steve Bob']);
+        $this->assertDatabaseMissing('invoices', ['billToName' => 'Steve Smith']);
     }
 }
